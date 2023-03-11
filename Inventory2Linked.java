5db64ef827dcd1c55cb1126763ce3fec0709b8ba
Errin Fulp <fulp@wfu.edu>
2023-03-11 11:10:23 -0500

    Initial commit

---
+++ Inventory2Linked.java
@@ -0,0 +1,138 @@
+/**
+ * CSC 112 Lab7 (Exceptionally Unique Linked Inventory) Spring 2023
+ * This program reads inventory and update files and creates a database (list)
+ * of unique ID numbers. Statistics about the database and file contents
+ * are then displayed. The file names are read from the user.
+ *
+ * @author Nirre Pluf {@literal <pluf@wfu.edu>}
+ * @version 0.1, Mar. 10, 2023
+ */
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Inventory2Linked {
+    public static void main(String[] args) {
+        String inFileName;      // inventory file name
+        String upFileName;      // update file name
+        Scanner keyboard = new Scanner(System.in); // keyboard
+        Scanner inFile = null;  // inventory file scanner
+        Scanner upFile = null;  // update file scanner
+
+        // read inFileName and upFileName from the user
+        System.out.print("Enter inventory file name and update file name  -> ");
+        inFileName = keyboard.next();
+        upFileName = keyboard.next();
+
+        inFile = openFileScanner(inFileName);
+        upFile = openFileScanner(upFileName);
+
+        var list = new LinkedList<Integer>();  // the database
+
+        // process inventory file
+        System.out.println("processing: " + inFileName);
+        readInventoryFile(inFile, list);
+        System.out.println(list);
+
+        // process the update file
+        System.out.println("processing: " + upFileName);
+        readUpdateFile(upFile, list);
+        System.out.println(list);
+    }
+
+
+    /**
+     * Opens a Scanner for the file called fileName, if unable the  
+     * method will exit the program.
+     *
+     * @param fileName String is the inventory file name
+     * @return Scanner object for the file fileName
+     */
+    private static Scanner openFileScanner(String fileName) {
+        Scanner fileScanner = null;
+        try {
+            fileScanner = new Scanner(new File(fileName));
+        } 
+        catch(FileNotFoundException e) {
+            System.out.println("Error in opening " + fileName);
+            System.exit(0);
+        }
+        return fileScanner;
+
+    }
+
+
+    /**
+     * Reads the contents of inFileName and stores the unique IDs in
+     * list. Maintains statistics about the items read.
+     *
+     * @param inFileName String is the inventory file name
+     * @param list LinkedList of unique IDs
+     */
+    private static void readInventoryFile(Scanner inFile, LinkedList<Integer>list) {
+        int id;         // id read from the file
+        int loc = 0;    // location of id in the list
+        int lines = 0;  // number of lines read from file
+
+        while(inFile.hasNext()) {
+            try {
+                id = inFile.nextInt();
+                if(id >= 0) {
+                    loc = list.indexOf(id);
+                    if(loc < 0) {
+                        list.addFirst(id);  
+                    }
+                }
+            }
+            catch(InputMismatchException e) {
+                inFile.next();
+            }
+            lines++;
+        }
+        System.out.println("lines: " + lines + ", unique: " + list.size());
+        inFile.close();
+    }
+
+
+    /**
+     * Reads the contents of upFileName and stores the unique IDs in
+     * list. Maintains statistics about the items read.
+     *
+     * @param upFileName String is the update file name
+     * @param list LinkedList of unique IDs
+     */
+    private static void readUpdateFile(Scanner upFile, LinkedList<Integer> list) {
+        char action;    // action code from file
+        int id;         // id from file
+        int loc = 0;    // location of id in the list
+        int lines = 0;  // number of lines read from file
+
+        while(upFile.hasNext()) {
+            try {
+                action = upFile.next().charAt(0);
+                id = upFile.nextInt();
+                if(Character.isLetter(action) && id >= 0) {
+                    if(id >= 0) {
+                        action = Character.toLowerCase(action);
+                        loc = list.indexOf(id);
+                        if(action == 'a' && loc < 0) {
+                            list.addFirst(id);
+                        }
+                        else if(action == 'd' && loc >= 0) {
+                            list.remove(loc);  
+                        }
+                    }
+                }
+            }
+            catch(InputMismatchException e) {
+                upFile.next();
+            }
+            lines++;
+        }
+        System.out.println("lines: " + lines + ", unique: " + list.size());
+        upFile.close();
+    }
+
+}
+
